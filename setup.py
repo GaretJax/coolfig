7b463d4b557c62d45c8c40c3f05d42abb396e89e
Jonathan Stoppani <jonathan@stoppani.name>
2018-08-23 13:22:49 +0200

    Format setup.py

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ import sys
 from setuptools import find_packages, setup
 
 
-PACKAGE = 'coolfig'
+PACKAGE = "coolfig"
 
 
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py sdist bdist_wheel upload')
+if sys.argv[-1] == "publish":
+    os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
 
 
@@ -25,12 +25,12 @@ class Setup(object):
         """
         try:
             filepath = os.path.join(os.path.dirname(__file__), fname)
-            with io.open(filepath, 'rt', encoding='utf8') as f:
+            with io.open(filepath, "rt", encoding="utf8") as f:
                 return f.read()
         except:
             if not fail_silently:
                 raise
-            return ''
+            return ""
 
     @classmethod
     def requirements(cls, fname):
@@ -38,10 +38,10 @@ class Setup(object):
         Create a list of requirements from the output of the pip freeze command
         saved in a text file.
         """
-        packages = cls.read(fname, fail_silently=True).split('\n')
+        packages = cls.read(fname, fail_silently=True).split("\n")
         packages = (p.strip() for p in packages)
-        packages = (p for p in packages if p and not p.startswith('#'))
-        packages = (p for p in packages if p and not p.startswith('https://'))
+        packages = (p for p in packages if p and not p.startswith("#"))
+        packages = (p for p in packages if p and not p.startswith("https://"))
         return list(packages)
 
     @staticmethod
@@ -50,8 +50,8 @@ class Setup(object):
         List all files in a data directory.
         """
         for base in bases:
-            basedir, _ = base.split('.', 1)
-            base = os.path.join(os.path.dirname(__file__), *base.split('.'))
+            basedir, _ = base.split(".", 1)
+            base = os.path.join(os.path.dirname(__file__), *base.split("."))
 
             rem = len(os.path.dirname(base)) + len(basedir) + 2
 
@@ -61,29 +61,32 @@ class Setup(object):
 
     @classmethod
     def meta(cls, key):
-        data = cls.read(os.path.join(PACKAGE, '__init__.py'))
-        version = (re.search(u"__{}__\s*=\s*u?('|\")([^']+)('|\")".format(key), data)
-                   .group(1).strip())
+        data = cls.read(os.path.join(PACKAGE, "__init__.py"))
+        version = (
+            re.search(u"__{}__\s*=\s*u?('|\")([^']+)('|\")".format(key), data)
+            .group(1)
+            .strip()
+        )
         return version
 
     @classmethod
     def version(cls):
-        return cls.meta('version')
+        return cls.meta("version")
 
     @classmethod
     def url(cls):
-        return cls.meta('url')
+        return cls.meta("url")
 
     @classmethod
     def longdesc(cls):
-        return cls.read('README.rst') + '\n\n' + cls.read('HISTORY.rst')
+        return cls.read("README.rst") + "\n\n" + cls.read("HISTORY.rst")
 
     @staticmethod
     def test_links():
         # Test if hardlinks work. This is a workaround until
         # http://bugs.python.org/issue8876 is solved
-        if hasattr(os, 'link'):
-            tempfile = __file__ + '.tmp'
+        if hasattr(os, "link"):
+            tempfile = __file__ + ".tmp"
             try:
                 os.link(__file__, tempfile)
             except OSError as e:
@@ -98,28 +101,30 @@ class Setup(object):
 
 Setup.test_links()
 
-setup(name=PACKAGE,
-      version=Setup.version(),
-      author='Jonathan Stoppani',
-      author_email='jonathan@stoppani.name',
-      include_package_data=True,
-      zip_safe=False,
-      url=Setup.url(),
-      license='MIT',
-      packages=find_packages(),
-      package_dir={PACKAGE: PACKAGE},
-      description='Helpers for dealing with application settings',
-      install_requires=Setup.requirements('requirements.txt'),
-      long_description=Setup.longdesc(),
-      entry_points=Setup.read('entry-points.ini', True),
-      classifiers=[
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-      ])
+setup(
+    name=PACKAGE,
+    version=Setup.version(),
+    author="Jonathan Stoppani",
+    author_email="jonathan@stoppani.name",
+    include_package_data=True,
+    zip_safe=False,
+    url=Setup.url(),
+    license="MIT",
+    packages=find_packages(),
+    package_dir={PACKAGE: PACKAGE},
+    description="Helpers for dealing with application settings",
+    install_requires=Setup.requirements("requirements.txt"),
+    long_description=Setup.longdesc(),
+    entry_points=Setup.read("entry-points.ini", True),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+)
